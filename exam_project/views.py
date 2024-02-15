58631e9ce890fc8b79e2de07d034a5769ed68607
AbdurakhmonKhasanov <akhdeveloper777@gmial.com>
2024-02-15 17:36:32 +0500

    Initial commit

---
+++ exam_project/views.py
@@ -0,0 +1,22 @@
+
+from apps.forms import UserForm
+from apps.models import User
+
+
+class UserListView(ListView):
+    template_name = 'apps/user_list.html'
+    paginate_by = 3
+    queryset = User.objects.order_by('-id')
+    context_object_name = 'users'
+
+
+class UserDeleteView(DeleteView):
+    model = User
+    pk_url_kwarg = 'pk'
+    form = UserForm
+    template_name = 'apps/user_list.html'
+    success_url = reverse_lazy('user_list_page')
+
+    def get(self, request, pk):
+        User.objects.filter(id=pk).delete()
+        return redirect('user_list_page')
